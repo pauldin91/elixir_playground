ad37b743a5817cda261f070d22c99661d1f2aab6
pauldin91@gmail.com <pauldin91@gmail.com>
2025-07-27 20:33:59 +0300

    umbrella projects && echo server

---
+++ exercism/lib/newsletter.ex
@@ -0,0 +1,32 @@
+defmodule Newsletter do
+  def read_emails(path) do
+    # Please implement the read_emails/1 function
+    {:ok, f} = File.open(path, [:read])
+    line_by_line(f)
+  end
+
+  def line_by_line(:eof, lines), do: lines
+
+  def line_by_line(file, lines \\ []) do
+    case IO.read(file, :line) do
+      :eof -> Enum.reverse(lines)
+      line -> line_by_line(file, [line | lines])
+    end
+  end
+
+  def open_log(path) do
+    # Please implement the open_log/1 function
+  end
+
+  def log_sent_email(pid, email) do
+    # Please implement the log_sent_email/2 function
+  end
+
+  def close_log(pid) do
+    # Please implement the close_log/1 function
+  end
+
+  def send_newsletter(emails_path, log_path, send_fun) do
+    # Please implement the send_newsletter/3 function
+  end
+end
