fafa3d467410f49f09da04b855a5a3c78be0d051
pauldin91@gmail.com <pauldin91@gmail.com>
2025-07-28 20:15:49 +0300

    matching brackets

--- exercism/lib/brackets.ex
+++ exercism/lib/brackets.ex
@@ -4,16 +4,26 @@ defmodule MatchingBrackets do
   """
   @spec check_brackets(String.t()) :: boolean
   def check_brackets(string) do
-    is_balanced?([h | t], 0)
+    is_balanced?(to_charlist(string), [])
   end
 
-  defp is_balanced?([], count), do: count == 0
+  defp is_balanced?([], list), do: length(list) == 0
 
-  defp is_balanced?([h | t], count) do
+  defp is_balanced?([h_src | t_src], list) do
     cond do
-      h == "{" or h == "[" or h == "(" -> is_balanced?(t, count + 1)
-      h == "}" or h == "]" or h == ")" -> is_balanced?(t, count - 1)
-      true -> count == 0
+      h_src == ?{ or h_src == ?[ or h_src == ?( ->
+        is_balanced?(t_src, [h_src | list])
+
+      (h_src == ?) and hd(list) == ?() or
+        (h_src == ?] and hd(list) == ?[) or
+          (h_src == ?} and hd(list) == ?{) ->
+        is_balanced?(t_src, tl(list))
+
+      t_src != [] && list != [] ->
+        is_balanced?(t_src, list)
+
+      true ->
+        length(list) == 0
     end
   end
 end
