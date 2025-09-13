b5c04c5e5add562869cab4c6521c8670ee355559
pauldin91 <pauldin91@gmail.com>
2025-09-13 11:15:00 +0300

    spiral draft

--- exercism/lib/spiral.ex
+++ exercism/lib/spiral.ex
@@ -7,5 +7,21 @@ defmodule Spiral do
   def matrix(1), do: [[1]]
 
   def matrix(dimension) do
+    for i <- 0..(dimension - 1), j <- 0..(dimension - 1) do
+      cond do
+        i == 0 ->
+          dimension * i + j + 1
+
+        j == dimension - 1 ->
+          i + j * dimension + 1
+
+        i == dimension - 1 ->
+          i * dimension - j - 1
+
+        true ->
+          i + j * dimension + 1
+      end
+    end
+    |> Enum.chunk_every(dimension)
   end
 end
