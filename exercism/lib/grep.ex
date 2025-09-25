f581c78a4b71455fea191b86fd6a5ddc41649a17
pauldin91 <pauldin91@gmail.com>
2025-09-25 21:50:03 +0300

    grep draft

--- exercism/lib/grep.ex
+++ exercism/lib/grep.ex
@@ -8,12 +8,50 @@ defmodule Grep do
     |> Enum.map(fn {n, s} ->
       {n,
        s
-       |> String.split("\n", trim: false)
-       |> Enum.filter(fn t -> String.contains?(t, pattern) end)}
+       |> String.split("\n")
+       |> Enum.with_index()
+       |> Enum.filter(fn {t, _} ->
+         res =
+           cond do
+             Enum.member?(flags, "-x") && Enum.member?(flags, "-i") ->
+               String.downcase(t) == String.downcase(pattern)
+
+             Enum.member?(flags, "-x") ->
+               t == pattern
+
+             Enum.member?(flags, "-i") ->
+               String.contains?(String.downcase(t), String.downcase(pattern))
+
+             true ->
+               String.contains?(t, pattern)
+           end
+
+         cond do
+           Enum.member?(flags, "-v") -> !res
+           true -> res
+         end
+       end)}
     end)
     |> Enum.reduce([], fn x, acc ->
-      acc ++ for(n <- elem(x, 1), do: elem(x, 0) <> ":" <> n)
+      acc ++
+        for(
+          n <- elem(x, 1),
+          do:
+            cond do
+              Enum.member?(flags, "-l") ->
+                elem(x, 0) <> "\n"
+
+              Enum.member?(flags, "-n") && Enum.member?(flags, "-l") ->
+                elem(x, 0) <> ":" <> "#{elem(n, 1)}:" <> elem(n, 0) <> "\n"
+
+              Enum.member?(flags, "-n") ->
+                elem(n, 1) <> ":" <> elem(n, 0) <> "\n"
+
+              true ->
+                elem(n, 0) <> "\n"
+            end
+        )
     end)
-    |> Enum.join("\n")
+    |> Enum.join()
   end
 end
