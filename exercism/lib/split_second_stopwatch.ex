f2d11946d905137d42647a072fdf7bdd83dc143c
pauldin91 <pauldin91@gmail.com>
2025-10-03 12:24:22 +0300

    stopwatch

--- exercism/lib/split_second_stopwatch.ex
+++ exercism/lib/split_second_stopwatch.ex
@@ -6,124 +6,96 @@ defmodule SplitSecondStopwatch do
   @type state :: :ready | :running | :stopped
 
   defmodule Stopwatch do
-    @type t :: :todo
-    defstruct [:state, :laps, :current_lap_ts]
+    @type t :: %__MODULE__{
+            state: SplitSecondStopwatch.state(),
+            current: Time.t(),
+            laps: [Time.t()]
+          }
+    defstruct [:state, :current, :laps]
   end
+
+  ## Helpers
+
+  defp zero(), do: ~T[00:00:00]
+
+  defp add_times(%Time{} = t1, %Time{} = t2) do
+    Time.add(t1, Time.diff(t2, zero()))
+  end
+
+  defp sum_times(times) do
+    times
+    |> Enum.reduce(zero(), fn t, acc -> add_times(acc, t) end)
+  end
+
+  ## API
 
   @spec new() :: Stopwatch.t()
   def new() do
-    %Stopwatch{laps: [], state: :ready}
+    %Stopwatch{state: :ready, current: zero(), laps: []}
   end
 
   @spec state(Stopwatch.t()) :: state()
-  def state(stopwatch) do
-    stopwatch.state
-  end
+  def state(sw), do: sw.state
 
   @spec current_lap(Stopwatch.t()) :: Time.t()
-  def current_lap(stopwatch) do
-    cond do
-      stopwatch.state == :running ->
-        track(stopwatch.current_lap_ts)
-
-      true ->
-        {:ok, time} = Time.new(0, 0, 0)
-        time
-    end
-  end
+  def current_lap(sw), do: sw.current
 
   @spec previous_laps(Stopwatch.t()) :: [Time.t()]
-  def previous_laps(stopwatch) do
-    stopwatch.laps
-  end
+  def previous_laps(sw), do: sw.laps
 
   @spec advance_time(Stopwatch.t(), Time.t()) :: Stopwatch.t()
-  def advance_time(stopwatch, time) do
-    cond do
-      stopwatch.state == :running ->
-        %Stopwatch{
-          laps: [track(stopwatch.current_lap_ts) | stopwatch.laps],
-          current_lap_ts: time,
-          state: stopwatch.state
-        }
-
-      true ->
-        stopwatch
-    end
+  def advance_time(%Stopwatch{state: :running} = sw, time) do
+    %{sw | current: add_times(sw.current, time)}
   end
 
-  @spec total(Stopwatch.t()) :: Time.t()
-  def total(stopwatch) do
-    cond do
-      stopwatch.state == :running ->
-        track(stopwatch.current_lap_ts)
+  def advance_time(sw, _), do: sw
 
-      true ->
-        {:ok, time} = Time.new(0, 0, 0)
-        time
-    end
+  @spec total(Stopwatch.t()) :: Time.t()
+  def total(%Stopwatch{state: :ready}), do: zero()
+
+  def total(%Stopwatch{laps: laps, current: current}) do
+    sum_times(laps ++ [current])
   end
 
   @spec start(Stopwatch.t()) :: Stopwatch.t() | {:error, String.t()}
-  def start(stopwatch) do
-    cond do
-      stopwatch.state == :ready ->
-        %Stopwatch{laps: stopwatch.laps, current_lap_ts: Time.utc_now(), state: :running}
+  def start(%Stopwatch{state: s}) when s != :ready and s != :stopped,
+    do: {:error, "cannot start an already running stopwatch"}
 
-      stopwatch.state == :stopped ->
-        %Stopwatch{
-          laps: stopwatch.laps,
-          current_lap_ts: Time.utc_now(),
-          state: :running
-        }
+  def start(%Stopwatch{state: :ready}) do
+    %Stopwatch{state: :running, current: zero(), laps: []}
+  end
 
-      true ->
-        {:error, "cannot start an already running stopwatch"}
-    end
+  def start(%Stopwatch{state: :stopped} = sw) do
+    %Stopwatch{
+      state: :running,
+      current: sw.current,
+      laps: sw.laps
+    }
   end
 
   @spec stop(Stopwatch.t()) :: Stopwatch.t() | {:error, String.t()}
-  def stop(stopwatch) do
-    cond do
-      stopwatch.state == :running ->
-        %Stopwatch{
-          laps: stopwatch.laps,
-          current_lap_ts: Time.utc_now(),
-          state: :stopped
-        }
+  def stop(%Stopwatch{state: s}) when s != :running,
+    do: {:error, "cannot stop a stopwatch that is not running"}
 
-      true ->
-        {:error, "cannot stop a stopwatch that is not running"}
-    end
+  def stop(%Stopwatch{state: :running} = sw) do
+    %{sw | state: :stopped}
   end
 
   @spec lap(Stopwatch.t()) :: Stopwatch.t() | {:error, String.t()}
-  def lap(stopwatch) do
-    cond do
-      stopwatch.state == :running ->
-        %Stopwatch{
-          laps: [
-            track(stopwatch.current_lap_ts)
-            | stopwatch.laps
-          ],
-          current_lap_ts: Time.utc_now(),
-          state: :running
-        }
 
-      true ->
-        {:error, "cannot lap a stopwatch that is not running"}
-    end
+  def lap(%Stopwatch{state: s}) when s != :running,
+    do: {:error, "cannot lap a stopwatch that is not running"}
+
+  def lap(%Stopwatch{state: :running, current: current, laps: laps} = sw) do
+    %{sw | laps: laps ++ [current], current: zero()}
   end
 
   @spec reset(Stopwatch.t()) :: Stopwatch.t() | {:error, String.t()}
-  def reset(stopwatch) do
-    cond do
-      stopwatch.state != :stopped -> {:error, "cannot reset a stopwatch that is not stopped"}
-      true -> %Stopwatch{laps: stopwatch.laps, state: :ready}
-    end
-  end
 
-  def track(time) do
-    Time.from_seconds_after_midnight(Time.diff(Time.utc_now(), time))
+  def reset(%Stopwatch{state: s}) when s != :stopped,
+    do: {:error, "cannot reset a stopwatch that is not stopped"}
+
+  def reset(%Stopwatch{state: :stopped}) do
+    %Stopwatch{state: :ready, current: zero(), laps: []}
   end
 end
