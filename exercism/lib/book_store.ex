b8f284a0fe938a179db0c401d252e7f3fb99a47d
pauldin91 <pauldin91@gmail.com>
2025-10-12 11:38:20 +0300

    circular buffer draft

--- exercism/lib/book_store.ex
+++ exercism/lib/book_store.ex
@@ -5,67 +5,32 @@ defmodule BookStore do
   @doc """
   Calculate lowest price (in cents) for a shopping basket containing books.
   """
+
+  @price_map %{1 => 800, 2 => 1520, 3 => 2160, 4 => 2560, 5 => 3000}
+
   @spec total(basket :: [book]) :: integer
   def total(basket) do
-    f = split_sets(basket |> Enum.frequencies() |> Map.to_list(), [])
-    Enum.map(f, &reduce_freqs(&1, 0)) |> Enum.sum()
+    f =
+      basket
+      |> Enum.frequencies()
+      |> split_sets()
+
+    Enum.map(f, &Map.get(@price_map, &1, 0)) |> Enum.sum()
   end
 
-  def reduce_freqs(prev, acc) do
-    cond do
-      Enum.count(prev) == 0 ->
-        acc
+  def split_sets(books, acc \\ [])
 
-      Enum.count(prev) == 1 ->
-        acc +
-          800 * Enum.sum_by(prev, fn {_, v} -> v end)
-
-      true ->
-        count = Enum.count(prev)
-
-        new_values =
-          Map.new(prev, fn {s, i} -> {s, i - 1} end) |> Map.filter(fn {_, i} -> i > 0 end)
-
-        reduce_freqs(
-          new_values,
-          acc +
-            count * (800 - calculate_disount(count))
-        )
-    end
+  def split_sets(books, acc) when map_size(books) == 0 do
+    if 3 not in acc or 5 not in acc,
+      do: acc,
+      else: split_sets(books, (acc -- [3, 5]) ++ [4, 4])
   end
-
-  def calculate_disount(books) do
-    cond do
-      books == 2 ->
-        div(800, 20)
-
-      books == 3 ->
-        div(800, 10)
-
-      books == 4 ->
-        div(800, 5)
-
-      true ->
-        div(800, 4)
-    end
-  end
-
-  def split_sets([], acc), do: acc |> Enum.filter(&(&1 != []))
 
   def split_sets(input, acc) do
-    valid = Enum.filter(input, fn {_, f} -> f > 0 end)
-    left = Enum.map(valid, fn {k, _} -> {k, 1} end)
-    right = Enum.map(valid, fn {k, f} -> {k, f - 1} end) |> Enum.filter(fn {_, f} -> f > 0 end)
+    right =
+      Enum.reject(input, fn {_, f} -> f == 0 end)
+      |> Enum.map(fn {k, f} -> {k, f - 1} end)
 
-    split_sets(right, [left | acc])
-  end
-
-  def do_split(left, right) do
-    s1 = left |> MapSet.new()
-    s2 = right |> MapSet.new()
-    diff = MapSet.symmetric_difference(s1, s2) |> MapSet.to_list()
-    intersection = MapSet.intersection(s1, s2)
-
-    {left, right}
+    split_sets(Map.new(right), [length(right) | acc])
   end
 end
--- exercism/lib/circular_buffer.ex
+++ exercism/lib/circular_buffer.ex
@@ -1,41 +1,72 @@
-# defmodule CircularBuffer do
-#   @moduledoc """
-#   An API to a stateful process that fills and empties a circular buffer
-#   """
+defmodule CircularBuffer do
+  @moduledoc """
+  An API to a stateful process that fills and empties a circular buffer
+  """
 
-#   @doc """
-#   Create a new buffer of a given capacity
-#   """
-#   @spec new(capacity :: integer) :: {:ok, pid}
-#   def new(capacity) do
-#     for(_n <- 1..capacity, do: [])
-#   end
+  @doc """
+  Create a new buffer of a given capacity
+  """
 
-#   @doc """
-#   Read the oldest entry in the buffer, fail if it is empty
-#   """
-#   @spec read(buffer :: pid) :: {:ok, any} | {:error, atom}
-#   def read(buffer) do
-#   end
+  defstruct [:buffer, :capacity, elements: 0]
+  @spec new(capacity :: integer) :: {:ok, pid}
+  def new(capacity) do
+    GenServer.start_link(__MODULE__, capacity)
+  end
 
-#   @doc """
-#   Write a new item in the buffer, fail if is full
-#   """
-#   @spec write(buffer :: pid, item :: any) :: :ok | {:error, atom}
-#   def write(buffer, item) do
-#   end
+  @doc """
+  Read the oldest entry in the buffer, fail if it is empty
+  """
+  @spec read(buffer :: pid) :: {:ok, any} | {:error, atom}
+  def read(buffer) do
+    GenServer.call(buffer, :read)
+  end
 
-#   @doc """
-#   Write an item in the buffer, overwrite the oldest entry if it is full
-#   """
-#   @spec overwrite(buffer :: pid, item :: any) :: :ok
-#   def overwrite(buffer, item) do
-#   end
+  @doc """
+  Write a new item in the buffer, fail if is full
+  """
+  @spec write(buffer :: pid, item :: any) :: :ok | {:error, atom}
+  def write(buffer, item) do
+    GenServer.cast(buffer, {:write, item})
+  end
 
-#   @doc """
-#   Clear the buffer
-#   """
-#   @spec clear(buffer :: pid) :: :ok
-#   def clear(buffer) do
-#   end
-# end
+  @doc """
+  Write an item in the buffer, overwrite the oldest entry if it is full
+  """
+  @spec overwrite(buffer :: pid, item :: any) :: :ok
+  def overwrite(buffer, item) do
+    GenServer.cast(buffer, {:overwrite, item})
+  end
+
+  @doc """
+  Clear the buffer
+  """
+  @spec clear(buffer :: pid) :: :ok
+  def clear(buffer) do
+    GenServer.call(buffer, :clear)
+  end
+
+  @impl true
+  def init(initial) do
+    {:ok, %CircularBuffer{buffer: [], capacity: initial}}
+  end
+
+  @impl true
+  def handle_call(:read, _from, buffer) do
+    cond do
+      buffer.elements == nil -> {:reply, {:error, :empty}, []}
+      true -> {:reply, hd(buffer.buffer), buffer}
+    end
+  end
+
+  @impl true
+  def handle_cast({:write, item}, buffer) do
+    cond do
+      buffer.elements == 0 ->
+        {:noreply, %CircularBuffer{buffer: [item], elements: 1, capacity: buffer.capacity}}
+
+      true ->
+        {:noreply,
+         %CircularBuffer{buffer: [item | buffer.buffer], elements: 1, capacity: buffer.capacity}}
+    end
+  end
+end
