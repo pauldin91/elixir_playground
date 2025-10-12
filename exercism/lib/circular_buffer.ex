c8278e21e02a7ad95b62454b68da53829b462d2f
pauldin91 <pauldin91@gmail.com>
2025-10-12 13:43:56 +0300

    circular buffer draft 2

--- exercism/lib/circular_buffer.ex
+++ exercism/lib/circular_buffer.ex
@@ -7,7 +7,7 @@ defmodule CircularBuffer do
   Create a new buffer of a given capacity
   """
 
-  defstruct [:buffer, :capacity, elements: 0]
+  defstruct [:buffer, :capacity, :starting_at]
   @spec new(capacity :: integer) :: {:ok, pid}
   def new(capacity) do
     GenServer.start_link(__MODULE__, capacity)
@@ -25,16 +25,32 @@ defmodule CircularBuffer do
   Write a new item in the buffer, fail if is full
   """
   @spec write(buffer :: pid, item :: any) :: :ok | {:error, atom}
-  def write(buffer, item) do
-    GenServer.cast(buffer, {:write, item})
+  def write(pid, item) do
+    buffer = GenServer.call(pid, :get_buffer)
+
+    cond do
+      Enum.count(buffer.buffer) == buffer.capacity ->
+        {:error, :full}
+
+      true ->
+        GenServer.cast(pid, {:write, item})
+    end
   end
 
   @doc """
   Write an item in the buffer, overwrite the oldest entry if it is full
   """
   @spec overwrite(buffer :: pid, item :: any) :: :ok
-  def overwrite(buffer, item) do
-    GenServer.cast(buffer, {:overwrite, item})
+  def overwrite(pid, item) do
+    buffer = GenServer.call(pid, :get_buffer)
+
+    cond do
+      Enum.count(buffer.buffer) < buffer.capacity ->
+        GenServer.cast(pid, {:write, item})
+
+      true ->
+        GenServer.cast(pid, {:overwrite, item})
+    end
   end
 
   @doc """
@@ -53,20 +69,49 @@ defmodule CircularBuffer do
   @impl true
   def handle_call(:read, _from, buffer) do
     cond do
-      buffer.elements == nil -> {:reply, {:error, :empty}, []}
-      true -> {:reply, hd(buffer.buffer), buffer}
+      buffer.starting_at == nil or buffer.buffer == [] ->
+        {:reply, {:error, :empty}, []}
+
+      true ->
+        [h | t] = buffer.buffer
+
+        cond do
+          t == [] -> {:reply, {:ok, h}, %CircularBuffer{buffer: [], capacity: buffer.capacity}}
+          true -> {:reply, {:ok, h}, %CircularBuffer{buffer | starting_at: hd(t), buffer: t}}
+        end
     end
+  end
+
+  def handle_call(:get_buffer, _from, buffer) do
+    {:reply, buffer, buffer}
+  end
+
+  def handle_call(:clear, _from, buffer) do
+    {:reply, :ok, %CircularBuffer{buffer | buffer: []}}
   end
 
   @impl true
   def handle_cast({:write, item}, buffer) do
     cond do
-      buffer.elements == 0 ->
-        {:noreply, %CircularBuffer{buffer: [item], elements: 1, capacity: buffer.capacity}}
+      buffer.starting_at == nil or buffer.buffer == [] ->
+        {:noreply, %CircularBuffer{buffer: [item], starting_at: item, capacity: buffer.capacity}}
 
       true ->
-        {:noreply,
-         %CircularBuffer{buffer: [item | buffer.buffer], elements: 1, capacity: buffer.capacity}}
+        {:noreply, %CircularBuffer{buffer | buffer: buffer.buffer ++ [item]}}
     end
+  end
+
+  def handle_cast({:overwrite, item}, buffer) do
+    {left, right} = Enum.split(buffer.buffer, buffer.starting_at)
+
+    IO.puts(inspect(left))
+    IO.puts(inspect(right))
+
+    {:noreply,
+     %CircularBuffer{
+       buffer
+       | starting_at: hd(right),
+         buffer: ([item | left |> Enum.reverse() |> tl] |> Enum.reverse()) ++ right
+     }}
   end
 end
