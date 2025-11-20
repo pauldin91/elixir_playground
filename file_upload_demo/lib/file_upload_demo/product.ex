5479da013d9e76b37ed9840728c4d18b3d61215c
pauldin91 <pauldin91@gmail.com>
2025-11-20 13:47:22 +0200

    changeset validataions etc

--- file_upload_demo/lib/file_upload_demo/product.ex
+++ file_upload_demo/lib/file_upload_demo/product.ex
@@ -14,7 +14,27 @@ defmodule FileUploadDemo.Product do
   def changeset(product, attrs) do
     product
     |> cast(attrs, [:name, :slug, :console])
-    |> validate_required([:name, :slug, :console])
+    |> validate_required([:name, :console])
+    |> validate_length(:name, min: 3)
+    |> format_name()
+    |> generate_slug()
     |> unique_constraint(:slug)
+  end
+
+  defp format_name(changeset) do
+    name =
+      changeset.changes.name
+      |> String.trim()
+
+    put_change(changeset, :name, name)
+  end
+
+  defp generate_slug(changeset) do
+    slug =
+      changeset.changes.name
+      |> String.downcase()
+      |> String.replace(" ", "-")
+
+    put_change(changeset, :slug, slug)
   end
 end
